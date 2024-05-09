2ebca53283f6696b303d22dce2e5492b48e16b6f
Rodrigo <rodrigo.brandao98@gmail.com>
2024-05-09 16:48:12 -0300

    class: Implementando repositórios do Prisma

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { z } from 'zod'
 import { JwtAuthGuard } from '@/infra/auth/jwt.guard'
 
