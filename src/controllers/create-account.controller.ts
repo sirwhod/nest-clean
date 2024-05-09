fe120483dc0c3a2dd81875093d4de3b39680b6a6
Rodrigo <rodrigo.brandao98@gmail.com>
2024-05-09 07:33:15 -0300

    style: realiza o lint do controller

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,5 +1,10 @@
-import { ConflictException } from '@nestjs/common';
-import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import {
+  ConflictException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('/accounts')
