fb03e8013a7d6b1d6e962aa81630a4761cbef87f
Rodrigo <rodrigo.brandao98@gmail.com>
2024-05-13 09:50:37 -0300

    class: Tratando erros nos controllers

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
 
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 
@@ -30,11 +36,15 @@ export class CreateQuestionController {
     const { title, content } = body
     const userId = user.sub
 
-    await this.createQuestion.execute({
+    const result = await this.createQuestion.execute({
       title,
       content,
       authorId: userId,
       attachmentsIds: [],
     })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
   }
 }
